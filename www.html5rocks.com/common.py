c485d302d24922426c40dc9c80ba981cf43262e5
Michael Mahemoff <mahemoff@html5rocks.com>
2010-12-15 00:14:05 +0000

    common.py includes profile.yaml parsing

---
+++ www.html5rocks.com/common.py
@@ -0,0 +1,12 @@
+import os
+import yaml
+
+def load_profiles():
+  basedir = os.path.dirname(__file__)
+  f = file(basedir + '/profiles.yaml', 'r')
+  profiles = dict()
+  for profile in yaml.load_all(f):
+    profiles[profile["id"]] = profile
+  f.close()
+  return profiles
+
--- www.html5rocks.com/static/js/profiles.js
+++ www.html5rocks.com/static/js/profiles.js
@@ -1,7 +1,15 @@
 $(function() {
+
   $(".profile").click(function(ev) {
     $(this).toggleClass("active");
     $(".profile").not(this).removeClass("active");
     return false;
   });
+
+  $(window).hashchange( function(){
+    $(".profile").removeClass("active");
+    $("[data-profile-id='"+location.hash+"']").addClass("active");
+  })
+  $(window).hashchange(); // run on page load
+
 });
