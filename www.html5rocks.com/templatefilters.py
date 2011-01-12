7447e0a688df856b8b7192ce188f2e47b228797f
Eric Bidelman <ericbidelman@html5rocks.com>
2011-01-11 16:33:19 -0800

    Prevent 500s if profile id doesn't exist

--- www.html5rocks.com/templatefilters.py
+++ www.html5rocks.com/templatefilters.py
@@ -58,11 +58,12 @@ class ProfileLink(django.template.Node):
   def render(self, context):
     names = []
     for id in self.ids:
-      profile = self.profiles[id]
-      names.append("<a href='/profiles/#!/%(id)s'>%(given)s %(family)s</a> - %(role)s, %(company)s" %
-          {'id': profile['id'], 'given': profile['name']['given'],
-           'family': profile['name']['family'], 'role': profile['org']['unit'],
-           'company': profile['org']['name']})
+      if id in self.profiles:
+        profile = self.profiles[id]
+        names.append("<a href='/profiles/#!/%(id)s'>%(given)s %(family)s</a> - %(role)s, %(company)s" %
+            {'id': profile['id'], 'given': profile['name']['given'],
+             'family': profile['name']['family'], 'role': profile['org']['unit'],
+             'company': profile['org']['name']})
     return ', '.join(names)
 
 
