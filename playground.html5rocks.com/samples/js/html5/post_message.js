fe97b4d76785b7d8b10257a5f23ddc0a91fc6585
Hanrui Gao <hanrui@html5rocks.com>
2011-01-26 16:14:24 +0800

    Move postmessage_origin.html to www.html5rocks.com.

--- playground.html5rocks.com/samples/js/html5/post_message.js
+++ playground.html5rocks.com/samples/js/html5/post_message.js
@@ -1,19 +1,20 @@
 document.body.innerHTML = "<p>Parent window (gray background)";
 document.body.style.backgroundColor = '#ddd';
 var iframe = document.createElement('iframe');
-iframe.src = 'http://apirocks.com/html5/playground_helper/postMessage_origin.html';
+iframe.src = 'http://www.html5rocks.com/static/demos/postmessage_origin.html';
 document.body.appendChild(iframe);
 
 
 window.addEventListener("message", function(e){
-  if (e.origin !== "http://apirocks.com" ) { // filter origin for security reasons
+  if (e.origin !== "http://www.html5rocks.com" ) { // filter origin for security reasons
     document.getElementById("log").innerHTML = 'The domain you are trying to interact with is not a valid origin.';
   } else {
     // you might as well do some filtering on e.data to be safe
     iframe.style.height = e.data['newHeight'] + 'px';
   }
+
   // if you were the first time caller you would need to use iframe.contentWindow.postMessage instead
-  e.source.postMessage('Iframe resized from the parent to adjust content', 'http://apirocks.com')
+  e.source.postMessage('Iframe resized from the parent to adjust content', 'http://www.html5rocks.com')
 }, false);
 
 /*
