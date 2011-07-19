6c92b82da45370d0da91fd57d01e46c8a639627f
Hanrui Gao <hanrui@html5rocks.com>
2011-07-19 15:59:19 -0700

    Remove mis-added line of importing users api

--- www.html5rocks.com/main.py
+++ www.html5rocks.com/main.py
@@ -44,7 +44,6 @@ from django.utils.translation import ugettext as _
 
 # Google App Engine Imports
 from google.appengine.api import memcache
-from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
