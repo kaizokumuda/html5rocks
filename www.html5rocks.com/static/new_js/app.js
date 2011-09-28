43d68f2763d912959b6f107586b632a57bddeeac
Matthew McVickar <matthewmcvickar@gmail.com>
2011-09-27 14:05:25 -1000

    Built the skeletal workings of the site grid navigation: a Javascript function is given the desired page, creates a DOM element for it, AJAXily stuffs the desired page into that new element, and scrolls there.

--- www.html5rocks.com/static/new_js/app.js
+++ www.html5rocks.com/static/new_js/app.js
@@ -1,3 +1,5 @@
+// Page header pulldowns.
+
 $('#search_show').click(function(){
   $('.subheader.features').hide();
   $('#features_show').removeClass('active');
@@ -21,3 +23,23 @@ $('#features_hide').click(function(){
   $('#features_show').removeClass('active');
   $('.subheader.features').hide();
 });
+
+$('.subheader.features ul li a').click(function(){
+  $('#features_show').removeClass('active');
+  $('.subheader.features').slideUp();
+});
+
+// Page grid builder
+
+function buildPageAndScrollThere()
+{
+  var newPage = $('<div id="newPage1"></div>');
+
+  $(newPage).html('new content!').css({'position': 'absolute', 'top': '3000px', 'left': '3000px', 'font-size': '48px', 'color': '#fff'});
+
+  $('body').append(newPage);
+
+  $(newPage).load('features/offline')
+
+  $.scrollTo({top:'3000px', left:'3000px'}, 800);
+}
