b83d7b733022bc30484001e635c410e209a376c6
Matthew McVickar <matthewmcvickar@gmail.com>
2011-10-07 02:45:34 -1000

    Added the feature navigation outline box, feature navigation outline box toggler, and feature paginator to the base navigation. Cleaned up all links that were pointing to # instead of having real URLs. Added 'resources.html' and 'why.html' path diviners to the main.py file and fixed all links that were still referencing the .html URLs.

--- www.html5rocks.com/static/new_js/app.js
+++ www.html5rocks.com/static/new_js/app.js
@@ -38,7 +38,7 @@ $('.subheader.features ul li a').click(function(){
   $('.subheader.features').slideUp();
 });
 
-// Page grid builder
+// Page grid builder.
 
 function buildPageAndScrollThere()
 {
@@ -52,3 +52,27 @@ function buildPageAndScrollThere()
     $.scrollTo({top:'3000px', left:'3000px'}, 800, {queue:true});
   });
 }
+
+// Features navigation.
+
+// Toggle the feature nav.
+$('.features_outline_nav_toggle').click(function(){
+  $(this).toggleClass('activated');
+  $('nav.features_outline').fadeToggle('fast');
+});
+
+// A feature is clicked.
+$('nav.features_outline a.section_title').click(function(){
+  if ($(this).parent('li').hasClass('current'))
+  {
+    $(this).parent('li').removeClass('current');
+    $(this).siblings('ul').slideUp('fast');
+  }
+  else
+  {
+    $('nav.features_outline li').removeClass('current');
+    $('nav.features_outline a.section_title').siblings('ul').slideUp('fast');
+    $(this).parent('li').addClass('current');
+    $(this).siblings('ul').slideDown('fast');
+  }
+});
