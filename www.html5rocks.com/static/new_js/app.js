50a8086375a6bf27af155b2b70088a4a873126a1
Eric Bidelman <ebidel@gmail.com>
2011-10-17 16:33:45 -0700

    - Toggle nav is visible and working - Anchor tags scroll in page properly

--- www.html5rocks.com/static/new_js/app.js
+++ www.html5rocks.com/static/new_js/app.js
@@ -1,11 +1,3 @@
-// +1 buttons.
-
-(function() {
-  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-  po.src = 'https://apis.google.com/js/plusone.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
-
 // Show header box shadow on scroll.
 var docTop = $('html, body').offset().top;
 $(window).bind('scroll', function(event) {
@@ -61,23 +53,49 @@ $('.subheader.features ul li a').click(function() {
 
 // Page grid navigation.
 
+function closeHeader() {
+  $('.subheader.features').slideUp();
+}
+
+function finishPanelLoad(pagePanel, opt_loadPlusOneButton) {
+  var loadPlusOneButton = opt_loadPlusOneButton || true;
+  // TODO(Google): scrollTo needs to scroll to and element that is not display:none.
+  // new.css applies this to .page elements. Not sure why pagePanel.addClass('current')
+  // doesn't take care of this.
+  $.scrollTo(pagePanel, 600, {queue: true, offset: {top: -60, left: 0}, onAfter: closeHeader});
+  if (loadPlusOneButton) {
+    gapi.plusone.go(pagePanel.find('.plusone').get(0));
+  }
+}
+
 $('a').click(function() {
   // Don't intercept external links
   if ($(this).attr('target')) {
     return true;
   }
 
+  // TODO: make sure this page var needs to be global.
   page = $(this).attr('href').substr($(this).attr('href').indexOf('/')).replace(/\/\w{2,3}\//gi, '').replace(/\/([A-Za-z]+)/gi, '-$1').replace(/\/$/, '').replace(/^-/, '');
+  page = page.split('#')[0];
 
   $('body').removeClass().attr('data-href', page);
   $('.page').removeClass('current');
 
   var pagePanel = $('.page#' + page);
+  pagePanel.addClass('current');
+
+  // If we have an anchor, just scroll to it on the current page panel.
+  var hash = $(this).attr('href').split('#')[1];
+  if (hash) {
+    finishPanelLoad(pagePanel.find('.' + hash), false);
+    return false;
+  }
+
   if (pagePanel.hasClass('loaded')) {
-    pagePanel.addClass('current');
+    finishPanelLoad(pagePanel);
   } else {
     //pagePanel.addClass('current loaded').load($(this).attr('href') + ' article');
-    pagePanel.addClass('current loaded');
+    pagePanel.addClass('loaded');
     $.ajax({
       url: $(this).attr('href'),
       success: function(data, textStatus, jqXHR) {
@@ -92,7 +110,7 @@ $('a').click(function() {
           eval(script.text); // TODO(ericbidelman): Figure out something better.
         });
 
-        $('.subheader.features').slideUp();
+        finishPanelLoad(pagePanel);
       }
     });
   }
@@ -132,20 +150,17 @@ $(document).keydown(function(e) {
 // Features navigation.
 
 // Toggle the feature nav.
-$('.features_outline_nav_toggle').click(function(){
+$('.features_outline_nav_toggle').click(function() {
   $(this).toggleClass('activated');
   $('nav.features_outline').fadeToggle('fast');
 });
 
 // A feature is clicked.
-$('nav.features_outline a.section_title').click(function(){
-  if ($(this).parent('li').hasClass('current'))
-  {
+$('nav.features_outline a.section_title').click(function() {
+  if ($(this).parent('li').hasClass('current')) {
     $(this).parent('li').removeClass('current');
     $(this).siblings('ul').slideUp('fast');
-  }
-  else
-  {
+  } else {
     $('nav.features_outline li').removeClass('current');
     $('nav.features_outline a.section_title').siblings('ul').slideUp('fast');
     $(this).parent('li').addClass('current');
