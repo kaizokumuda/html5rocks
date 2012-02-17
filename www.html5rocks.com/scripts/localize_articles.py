248b9f99fe326a48bb2a1058e0b0108433aa8346
Mike West <mike@mikewest.org>
2012-02-17 15:06:04 +0100

    More or less on par with what's in trunk.

--- www.html5rocks.com/scripts/localize_articles.py
+++ www.html5rocks.com/scripts/localize_articles.py
@@ -28,10 +28,7 @@ import re
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class TextProcessor(object):
-  """A singleton that provides text processing methods to move from Django
-     templates to localizable HTML, and from localized HTML to Django template
-     format.
-  """
+  """Translates text from Django template format to localizable HTML and back."""
   
   UNTRANSLATABLE_BEGIN = r'<!--DO_NOT_TRANSLATE_BLOCK>'
   UNTRANSLATABLE_END = r'</DO_NOT_TRANSLATE_BLOCK-->'
@@ -43,6 +40,7 @@ class TextProcessor(object):
 <!--/END_CONTENT_BLOCK ******************************************************-->
 """
 
+  @classmethod
   def django_to_html(self, text):
     """Given a Django template's content, return HTML suitable for l10n.
 
@@ -100,6 +98,7 @@ class TextProcessor(object):
 
     return ''.join(to_return)
 
+  @classmethod
   def html_to_django(self, text):
     """Given localized HTML, return text formatted as a Django template.
 
@@ -143,216 +142,126 @@ class Article(object):
     locales: The article's available completed localizations.
   """
 
+  ROOT = os.path.abspath(os.path.join(ROOT_DIR, '..', 'content'))
+  UNLOCALIZED_ROOT = os.path.abspath(os.path.join(ROOT_DIR, '..',
+                                                  '_unlocalized'))
+  PATH_DELIMITER = '___===___'
+
   def __init__(self, path):
     if not os.path.exists(path):
       raise ArticleException('`%s` does not exist.' % path)
-
     self.path = path
-
+    self._locales = None
 
   @property
   def locales(self):
+    """Returns the article's localizations.
 
+    We assume that each of the subdirectories under |path| that contain an
+    `index.html` file are localizations. This getter scans the filesystem
+    to generate that list, then caches it for future calls.
 
+    Returns:
+      A list of locales, e.g. ['de','en','es']
+    """
+    if not self._locales:
+      self._locales = [dir
+                       for dir in os.listdir(self.path)
+                       if os.path.exists(os.path.join(self.path, dir,
+                                                      'index.html'))]
+    return self._locales
 
+  @property
+  def localizable_file_path(self):
+    """The path to an article's localizable representation.
 
+    Unlocalized articles live under `{Article.UNLOCALIZED_ROOT}`, and are named
+    by jamming the elements of the article's path together into one reversable
+    string (the localization system doesn't like subdirectories).
 
-# class Article(object):
-  # """Represents an article, and maps back and forth between Django and HTML.
+    `{Article.ROOT}/path/to/article` would return
+    `{Article.UNLOCALIZED_ROOT}/path__to__article.html` (where `__` represents
+    `Article.PATH_DELIMITER`).
 
-  # Attributes:
-    # DJANGO_ROOT: The root directory in which Django templates are found.
-    # UNLOCALIZED_ROOT: The root directory in which unlocalized files are found .
-    # django_: The absolute path to a specific Django template.
-  # """
-  # DJANGO_ROOT = os.path.abspath(os.path.join(ROOT_DIR, '..', 'content'))
-  # UNLOCALIZED_ROOT = os.path.abspath(os.path.join(ROOT_DIR, '..', '_to_localize'))
-  # LOCALIZED_ROOT = os.path.abspath(os.path.join(ROOT_DIR, '..', '_localized'))
+    Returns:
+      An absolute path to the article's localizable representation.
+    """
+    temp = re.sub(r'^%s/' % self.ROOT, '', self.path)
+    temp = re.sub(r'/', self.PATH_DELIMITER, temp)
+    return os.path.abspath(os.path.join(self.UNLOCALIZED_ROOT, temp))
 
+  def generate_localizable_file(self):
+    """Generates a localizable representation of the article.
 
-  # PATH_DELIMITER = """___---___"""
+    This method grabs the English version of the article, runs the text through
+    `TextProcessor.django_to_html`, and writes the result to
+    `localizableFilePath`.
 
-  # def __init__(self, original_file=None):
-    # """Creates an Article object.
+    Returns:
+      An absolute path to the article's localizable representation.
+    """
+    if not 'en' in self.locales:
+      raise ArticleException('`%s` has no English edition.' % self.path)
 
-    # Args:
-      # file: The path to either a Django file, or a localized template.
-    # """
-    # if original_file.startswith(Article.DJANGO_ROOT):
-      # # We've been given a Django file, s
-      # self.original_type_ = Article.DJANGO
-    # elif original_file.startswith(Article.LOCALIZED_ROOT):
-      # self.original_type_ = Article.LOCALIZED
+    original = os.path.join(self.ROOT, self.path, 'en', 'index.html')
+    with open(original, 'r') as input:
+      with open(self.localizable_file_path, 'w') as output:
+        output.write(TextProcessor.django_to_html(input.read()))
+    
+    return self.localizable_file_path 
 
-  # def GetUnlocalizedPath(self, django_path):
-    # """Given the path to a Django template, returns the path to a corresponding
-       # unlocalized file.
+class Localizer(object):
+  """Implements the HTML5Rocks article localization workflow.
 
-    # Args:
-      # django_file: The path to a Django template.
-    # """
-    # temp = re.sub(r'^%s/' % Article.DJANGO_ROOT, '', django_path)
-    # temp = re.sub(r'/', Article.PATH_DELIMITER, temp)
-    # return os.path.abspath(os.path.join(Article.UNLOCALIZED_ROOT, temp))
+  Given a root directory, Localizer looks for article files (defined as pretty
+  much anything living directly under an `en` directory (which is very fragile
+  and assumes we'll never have articles written first in languages other than
+  English (which is probably accurate, but not something we should hard-code)))
+  and converts the Django templates into HTML files that can be
+  passed into a translation system (by commenting out all the Django bits).
 
-  # def GetLocalizedPath(self, locale, html_path):
-    # """Given the path to a localized HTML file, returns the path to a
-       # corresponding Django file.
+  Localizer also goes in the other direction, converting localized HTML files
+  into Django-template counterparts that can be rendered as part of HTML5Rocks.
+  """
 
-    # Args"""
+  def __init__(self, original_root=None, localized_root=None):
+    """Constructs a Localizer object.
 
-  # def GenerateHTML(self):
-    # """Generates a localizable HTML file from a Django template.
+    Args:
+      original_root: The directory which Localizer should scan for original
+          articles.
+      localized_root: The directory Localizer should scan for finished
+          localizations.
+    """
+    self._original_root = original_root
+    self._localized_root = localized_root
+    self._articles = None
 
-    # This function will also create the HTML file's directory if it doesn't
-    # already exist.
-    # """
-    # try:
-      # os.makedirs(os.path.dirname(self.html_))
-    # except os.error:
-      # pass
-    # print '* Generating `%s`' % self.html_
-    # with open(self.html_, 'w') as outfile:
-      # with open(self.django_, 'r') as infile:
-        # in_content = False
-        # for line in infile:
-          # (line, in_content) = self._HtmlizeLine(line, in_content)
-          # outfile.write(line)
+  def _index_english_articles(self):
+    """Scans the original_root directory for English articles.
 
-  # def _HtmlizeLine(self, line, in_content):
-    # """Given a line of a Django template, returns the corresponding HTML line
+    Populates `self.articles_` with a list of Article objects representing
+    each.
 
-    # * Replaces Django tags with comments in the form:
-        # <!--DO_NOT_TRANSLATE_BLOCK-->{DJANGO_TAG}<!--/DO_NOT_TRANSLATE_BLOCK-->
+    Returns:
+        list of Article objects
+    """
+    self._original_articles = []
+    for root, _, files in os.walk(self._original_root):
+      for name in files:
+        if not name == '.DS_Store' and re.search(r'\/en$', root):
+          self._original_articles.append(Article(os.path.dirname(root)))
+    return self._original_articles
 
-    # * Replaces script/style/pre tags with comments in the form:
-        # <TAG_GOES_HERE><!--DO_NOT_TRANSLATE_BLOCK-->
-        # ...
-        # <!--/DO_NOT_TRANSLATE_BLOCK--></TAG_GOES_HERE>
-    # """
+  @property
+  def original_articles(self):
+    if self._articles is None:
+      self._index_english_articles()
+    return self._original_articles
 
-    # UNESCAPED_DJANGO = r'(?P<tag>{%.+?%})'
-    # ESCAPED_DJANGO = r'%s\g<tag>%s' % (Article.UNTRANSLATABLE_LABEL,
-                                       # Article.UNTRANSLATABLE_END_LABEL,)
-
-    # UNESCAPED_UNTRANSLATABLE = r'(?P<tag><(?:pre|script|style)[^>]*?>)'
-    # ESCAPED_UNTRANSLATABLE = r'\g<tag>%s' % Article.UNTRANSLATABLE_LABEL
-
-    # UNESCAPED_UNTRANSLATABLE_END = r'(?P<tag></(?:pre|script|style)[^>]*?>)'
-    # ESCAPED_UNTRANSLATABLE_END = r'%s\g<tag>' % Article.UNTRANSLATABLE_END_LABEL
-
-    # UNESCAPED_CONTENT = r'{% block content %}'
-    # UNESCAPED_CONTENT_END = r'{% endblock %}'
-
-    # # Preprocess Django tags
-    # line = re.sub(UNESCAPED_DJANGO, ESCAPED_DJANGO, line)
-    # # Preprocess script/pre/style blocks
-    # line = re.sub(UNESCAPED_UNTRANSLATABLE, ESCAPED_UNTRANSLATABLE, line)
-    # line = re.sub(UNESCAPED_UNTRANSLATABLE_END,
-                  # ESCAPED_UNTRANSLATABLE_END,
-                  # line)
-    # # Preprocess content block
-    # if not in_content and re.search(UNESCAPED_CONTENT, line):
-      # line = Article.CONTENT_LABEL
-      # in_content = True
-    # if in_content and re.search(UNESCAPED_CONTENT_END, line):
-      # line = Article.CONTENT_LABEL_END
-      # in_content = False
-    # return (line, in_content)
-
-  # def GenerateDjango(self):
-    # """Generates a Django template from a localized HTML file.
-
-    # Implemented trivially by stripping comments in the form:
-    # `<!--DJANGO>...</DJANGO-->`. This function will also create the Django
-    # template's directory if it doesn't already exist.
-    # """
-    # try:
-      # os.makedirs(os.path.dirname(self.django_))
-    # except os.error:
-      # pass
-    # with open(self.html_, 'r') as infile:
-      # with open(self.django_, 'w') as outfile:
-        # for line in infile:
-          # # Preprocess Django tags
-          # line = re.sub(Article.ESCAPED_DJANGO, Article.UNESCAPED_DJANGO, line)
-          # # Preprocess script blocks
-          # line = line.replace(Article.ESCAPED_SCRIPT, Article.UNESCAPED_SCRIPT)
-          # line = line.replace(Article.ESCAPED_SCRIPT_END,
-                              # Article.UNESCAPED_SCRIPT_END)
-          # # Preprocess style blocks
-          # line = line.replace(Article.ESCAPED_STYLE, Article.UNESCAPED_STYLE)
-          # line = line.replace(Article.ESCAPED_STYLE_END,
-                              # Article.UNESCAPED_STYLE_END)
-          # outfile.write(line)
-
-
-  # def __str__(self):
-    # """String representation of an Article."""
-
-    # return ('Article:\n'
-            # '- HTML Path:   `%s`\n'
-            # '- Django Path: `%s`\n') % (self.html_, self.django_)
-
-
-# class Localizer(object):
-  # """Implements the HTML5Rocks article localization workflow.
-
-  # Given a root directory, Localizer looks for article files (defined as pretty
-  # much anything living directly under an `en` directory (which is very fragile
-  # and assumes we'll never have articles written first in languages other than
-  # English (which is probably accurate, but not something we should hard-code)))
-  # and converts the Django templates into HTML files that can be
-  # passed into a translation system (by commenting out all the Django bits).
-
-  # Localizer also goes in the other direction, converting localized HTML files
-  # into Django-template counterparts that can be rendered as part of HTML5Rocks.
-
-  # Attributes:
-    # root_: The root directory from which Localizer scans
-    # articles_: A list of Article objects found during a scan.
-  # """
-
-  # def __init__(self, root):
-    # """Constructs a Localizer object.
-
-    # Args:
-      # root: The root directory from which Localizer should scan.
-    # """
-    # self.root_ = root
-    # self.articles_ = []
-
-  # def IndexEnglishArticles(self):
-    # """Scans the root directory for English articles.
-
-    # Populates `self.articles_` with a list of Article objects representing
-    # each.
-
-    # Returns:
-        # list of Article objects
-    # """
-    # self.articles_ = []
-    # for root, _, files in os.walk(Article.DJANGO_ROOT):
-      # for name in files:
-        # if not name == '.DS_Store' and re.search(r'\/en$', root):
-          # self.articles_.append(Article(django=os.path.join(root,
-                                                            # name)))
-    # return self.articles_
-
-  # def Htmlize(self):
-    # """Generates localizable HTML from Django templates."""
-
-    # print ('Generating Localizable HTML\n'
-           # '===========================\n')
-    # for article in self.articles_:
-      # article.GenerateHTML()
-
-  # def Djangoize(self):
-    # """Generates Django templates from localized HTML."""
-
-    # # TODO(mkwst): I should implement this function. :)
-    # print 'Not finished (read: started) yet. Come back later.'
-
+  def generate_localizable_files(self):
+    for article in self.original_articles:
+      article.generate_localizable_file()
 
 # def main():
   # parser = optparse.OptionParser()
--- www.html5rocks.com/scripts/localize_articles_test.py
+++ www.html5rocks.com/scripts/localize_articles_test.py
@@ -16,80 +16,218 @@
 
 """Unit Tests for the article l10n script."""
 
-import unittest
 import os
-from localize_articles import TextProcessor
+import tempfile
+import unittest
+
+from localize_articles import TextProcessor, Article, Localizer
+
+TEST_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 class TestTextProcessor(unittest.TestCase):
-  def setUp(self):
-    self._proc = TextProcessor()
-
   def test_identity(self):
     text = 'Test.'
-    self.assertEqual(text, self._proc.django_to_html(text))
+    self.assertEqual(text, TextProcessor.django_to_html(text))
 
   def test_tag(self):
     text = '{% tag %}'
     expected = r'%s{%% tag %%}%s' % (TextProcessor.UNTRANSLATABLE_BEGIN,
                                      TextProcessor.UNTRANSLATABLE_END)
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
 
   def test_tags(self):
     text = '{% tag1 %}{% tag2 %}'
     expected = '{0}{{% tag1 %}}{1}{0}{{% tag2 %}}{1}'.format(
         TextProcessor.UNTRANSLATABLE_BEGIN,
         TextProcessor.UNTRANSLATABLE_END)
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
 
   def test_content(self):
     text = '{% block content %}'
     expected = TextProcessor.CONTENT_BEGIN
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
 
   def test_content_end(self):
     text = '{% block content %}\n{% endblock %}'
     expected = '%s%s' % (TextProcessor.CONTENT_BEGIN,
                          TextProcessor.CONTENT_END)
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
 
   def test_endblock_outside_content(self):
     text = '{% endblock %}'
     expected = r'%s{%% endblock %%}%s' % (TextProcessor.UNTRANSLATABLE_BEGIN,
                                           TextProcessor.UNTRANSLATABLE_END)
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
 
   def test_untranslated_tags(self):
     text = '<pre>'
     expected = '<pre>%s' % TextProcessor.UNTRANSLATABLE_BEGIN
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
     text = '<script>'
     expected = '<script>%s' % TextProcessor.UNTRANSLATABLE_BEGIN
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
     text = '<style>'
     expected = '<style>%s' % TextProcessor.UNTRANSLATABLE_BEGIN
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
 
   def test_untranslated_tags_end(self):
     text = '</pre>'
     expected = '%s</pre>' % TextProcessor.UNTRANSLATABLE_END
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
     text = '</script>'
     expected = '%s</script>' % TextProcessor.UNTRANSLATABLE_END
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
     text = '</style>'
     expected = '%s</style>' % TextProcessor.UNTRANSLATABLE_END
-    self.assertEqual(expected, self._proc.django_to_html(text))
-    self.assertEqual(text, self._proc.html_to_django(expected))
+    self.assertEqual(expected, TextProcessor.django_to_html(text))
+    self.assertEqual(text, TextProcessor.html_to_django(expected))
+
+class TestArticleLocales(unittest.TestCase):
+  ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+  def test_article_locales_none(self):
+    temp = Article(os.path.join(TEST_ROOT, 'test_fixtures',
+                                'article_locales_none'))
+    self.assertEqual([], temp.locales)
+
+  def test_article_locales_files(self):
+    temp = Article(os.path.join(TEST_ROOT, 'test_fixtures',
+                                'article_locales_files'))
+    self.assertEqual([], temp.locales)
+
+  def test_article_locales_single(self):
+    temp = Article(os.path.join(TEST_ROOT, 'test_fixtures',
+                                'article_locales_single'))
+    self.assertEqual(['en'], temp.locales)
+
+  def test_article_locales_multiple(self):
+    temp = Article(os.path.join(TEST_ROOT, 'test_fixtures',
+                                'article_locales_multiple'))
+    self.assertEqual(['de', 'en', 'es'], temp.locales)
+
+  def test_article_locales_multiple_no_index(self):
+    temp = Article(os.path.join(TEST_ROOT, 'test_fixtures',
+                                'article_locales_multiple_no_index'))
+    self.assertEqual(['de', 'es'], temp.locales)
+
+class TestArticlePaths(unittest.TestCase):
+  ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+  def setUp(self):
+    # A little bit of monkey patching never hurt nobody.
+    Article.ROOT = TEST_ROOT
+
+  def test_article_path(self):
+    temp = Article(os.path.join(TEST_ROOT, 'test_fixtures', 'article_path'))
+    expected = os.path.join(Article.UNLOCALIZED_ROOT,
+                            Article.PATH_DELIMITER.join(
+                                ['test_fixtures', 'article_path']))
+    self.assertEqual(expected, temp.localizable_file_path)
+
+  def test_article_multiple(self):
+    temp = Article(os.path.join(TEST_ROOT, 'test_fixtures',
+                                'article_path_multiple', 'path1', 'path2'))
+    expected = os.path.join(Article.UNLOCALIZED_ROOT,
+                            Article.PATH_DELIMITER.join(
+                                ['test_fixtures', 'article_path_multiple',
+                                 'path1', 'path2']))
+    self.assertEqual(expected, temp.localizable_file_path)
+
+
+class TestArticleUnlocalizedGeneration(unittest.TestCase):
+  def setUp(self):
+    # A little bit of monkey patching never hurt nobody.
+    Article.ROOT = TEST_ROOT
+    Article.UNLOCALIZED_ROOT = tempfile.mkdtemp(prefix='Unlocalized_')
+    self._created_file = None
+
+  def tearDown(self):
+    if self._created_file:
+      os.remove(self._created_file)
+    os.removedirs(Article.UNLOCALIZED_ROOT)
+
+  def test_simple(self):
+    temp = Article(os.path.join(TEST_ROOT, 'test_fixtures',
+                                'article_locales_single'))
+    self._created_file = temp.generate_localizable_file()
+    self.assertEqual(temp.localizable_file_path, self._created_file)
+    self.assertTrue(os.path.exists(temp.localizable_file_path))
+    self.assertTrue(os.path.isfile(temp.localizable_file_path))
+
+class TestLocalizerIndex(unittest.TestCase):
+  def test_simple(self):
+    l7r = Localizer(original_root=os.path.join(TEST_ROOT, 'test_fixtures',
+                                               'localizer_simple'))
+    self.assertEqual(3, len(l7r.original_articles))
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_simple', 'article1'),
+                     l7r.original_articles[0].path)
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_simple', 'article2'),
+                     l7r.original_articles[1].path)
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_simple', 'article3'),
+                     l7r.original_articles[2].path)
+
+  def test_deeper(self):
+    l7r = Localizer(original_root=os.path.join(TEST_ROOT, 'test_fixtures',
+                                               'localizer_deeper'))
+    self.assertEqual(6, len(l7r.original_articles))
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_deeper', 'path1', 'path2',
+                                  'article1'),
+                     l7r.original_articles[0].path)
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_deeper', 'path1', 'path2',
+                                  'article2'),
+                     l7r.original_articles[1].path)
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_deeper', 'path1', 'path2',
+                                  'article3'),
+                     l7r.original_articles[2].path)
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_deeper', 'path3', 'path4',
+                                  'article4'),
+                     l7r.original_articles[3].path)
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_deeper', 'path3', 'path4',
+                                  'article5'),
+                     l7r.original_articles[4].path)
+    self.assertEqual(os.path.join(TEST_ROOT, 'test_fixtures',
+                                  'localizer_deeper', 'path3', 'path4',
+                                  'article6'),
+                     l7r.original_articles[5].path)
+
+class TestLocalizerLocalizableFileGeneration(unittest.TestCase):
+  def setUp(self):
+    # A little bit of monkey patching never hurt nobody.
+    Article.ROOT = TEST_ROOT
+    Article.UNLOCALIZED_ROOT = tempfile.mkdtemp(prefix='Unlocalized_')
+    self._created_files = []
+
+  def tearDown(self):
+    for file in self._created_files:
+      os.remove(file)
+    os.removedirs(Article.UNLOCALIZED_ROOT)
+
+  def test_simple(self):
+    l7r = Localizer(original_root=os.path.join(TEST_ROOT, 'test_fixtures',
+                                               'localizer_simple'))
+    l7r.generate_localizable_files()
+    for article in l7r.original_articles:
+      self._created_files.append(article.localizable_file_path)
+      self.assertTrue(os.path.exists(article.localizable_file_path))
+      self.assertTrue(os.path.isfile(article.localizable_file_path))
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
