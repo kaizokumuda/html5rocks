86925e398917690468fd8f2c9b29150588ce6bd8
Mike West <mike@mikewest.org>
2012-02-20 13:40:07 +0100

    Casestudies.

--- www.html5rocks.com/main.py
+++ www.html5rocks.com/main.py
@@ -284,63 +284,20 @@ class ContentHandler(webapp.RequestHandler):
         p['tuts_by_author'] = models.Resource.get_tutorials_by_author(p['id'])
       self.render(data={'sorted_profiles': profiles},
                   template_path='content/profiles.html', relpath=relpath)
-
-    elif re.search('tutorials/casestudies', relpath) and not is_feed:
-      # Case Studies look like this on the filesystem:
-      #
-      #   .../tutorials +
-      #                 |
-      #                 +-- casestudies   +
-      #                 |                 |
-      #                 |                 +-- en  +
-      #                 |                 |       |
-      #                 |                 |       +-- case_study_name.html
-      #                 ...
-      #
-      # So, to determine if an HTML page exists for the requested language
-      # `split` the file's path, add in the locale, and check existance:
-      logging.info('Building request for casestudy `%s` in locale `%s`',
-                   path, locale)
-      potentialfile = re.sub('tutorials/casestudies',
-                             'tutorials/casestudies/%s' % locale, path)
-      englishfile = re.sub('tutorials/casestudies',
-                           'tutorials/casestudies/%s' % 'en', path)
-
-      logging.info(englishfile)
-
-      if os.path.isfile(potentialfile):
-        logging.info('Rendering in native: %s' % potentialfile)
-
-        tut = models.Resource.all().filter('url =', '/' + relpath).get()
-
-        # If tutorial is marked as draft, redirect and don't show it.
-        if tut.draft:
-          return self.redirect('/tutorials')
-
-        data = {
-          'tut': tut,
-          'redirect_from_locale': redirect_from_locale
-        }
-
-        self.render(template_path=potentialfile, data=data, relpath=relpath)
-
-      # If the localized file doesn't exist, and the locale isn't English, look
-      # for an english version of the file, and redirect the user there if
-      # it's found:
-      elif os.path.isfile(englishfile):
-        return self.redirect("/en/%s?redirect_from_locale=%s" % (relpath,
-                                                                 locale))
-
     elif ((re.search('tutorials/.+', relpath) or
            re.search('mobile/.+', relpath) or
            re.search('gaming/.+', relpath) or
-           re.search('business/.+', relpath))
+           re.search('business/.+', relpath) or
+           re.search('tutorials/casestudies/.+', relpath))
           and not is_feed):
-      # If this is an old-style Mobile article, redirect to the new style.
-      match = re.search('mobile/(?P<slug>[a-z-_]+).html$', relpath)
+      # If this is an old-style mobile article or case study, redirect to the
+      # new style.
+      match = re.search(('(?P<type>mobile|tutorials/casestudies)'
+                         '/(?P<slug>[a-z-_]+).html$'), relpath)
       if match:
-        logging.info("Redirecting from old-style Mobile URL")
-        return self.redirect('/%s/mobile/%s/' % (locale, match.group('slug')))
+        logging.info("Redirecting from old-style URL to the new hotness.")
+        return self.redirect('/%s/%s/%s/' % (locale, match.group('type'),
+                                             match.group('slug')))
 
       # If no trailing / (e.g. /tutorials/blah), redirect to /tutorials/blah/.
       if (relpath[-1] != '/' and not relpath.endswith('.html')):
