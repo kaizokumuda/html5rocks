da7bdb05d77d7f99b934332614ce3c24076d23e1
Paul Irish <paul.irish@gmail.com>
2012-05-25 09:56:44 -0700

    adding in paul lewis's rAF++ article. first draft. with markdown build script too.

---
+++ www.html5rocks.com/content/tutorials/speed/animations/en/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Remove original index.
+rm index.html
+# Process some stuff.
+cat header.html >> index.html
+kramdown index.md >> index.html
+cat footer.html >> index.html
+# Pretty print everything.
+sed -ie 's/<pre>/<pre class="prettyprint">/' index.html
