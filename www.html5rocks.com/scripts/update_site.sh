94c8de4db1934ea9e50f7ede80fc571afe0b57dd
Eric Bidelman <ebidel@gmail.com>
2012-07-18 16:45:43 -0700

    Better release script that automates pretty everything

--- www.html5rocks.com/scripts/update_site.sh
+++ www.html5rocks.com/scripts/update_site.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 #
-# Runs compress_js_css.sh before uploading the app to App Engine.
+# Compresses the site's JS/CSS and cache busts links before uploading the app
+# to App Engine.
 # 
 # Note: This script should be used in place of using appcfg.py update directly
 # to update the application on App Engine.
 #
-# Copyright 2011 Eric Bidelman <ericbidelman@chromium.org>
+# Copyright 2012 Eric Bidelman <ericbidelman@html5rocks.com>
+
+for arg in $@
+do
+ if [ $arg = "--release" ]
+ then
+   versionStr=v`date +%Y%m%d`
+   
+   git checkout -b $versionStr
+
+   # Change app.yaml version to current date timestamp.
+   fl=../app.yaml
+   mv $fl $fl.old
+   sed "s/version: master/version: $versionStr/g" $fl.old > $fl
+   rm -f $fl.old
+
+   # Commit the new release branch.
+   git commit -m "Cutting release $versionStr" ../app.yaml
+   git push origin $versionStr
+ fi
+done
 
 ./compress_js_css.sh
 ./cachebust.py
 echo \# `date` >> ../cache.appcache
 appcfg.py update ../
+
+# Clean up our modified cache busted files and switch back to master.
+git checkout ../
+git checkout master
